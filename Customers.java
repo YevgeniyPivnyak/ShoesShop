e18e40d5b29cf1c4f05e595a45e574f3e40debdf
YevgeniyPivnyak <dneprojek@gmail.com>
2016-04-09 17:32:13 +0300

    add Customer, Gender, SaleInfo classes

---
+++ Customers.java
@@ -0,0 +1,31 @@
+
+public class Customers {
+
+    private String name;
+    private String phone;
+    private Gender gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
