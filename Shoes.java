f3d2be04e7389c23739b1018735ec6860243b624
YevgeniyPivnyak <dneprojek@gmail.com>
2016-04-09 17:03:42 +0300

    add Getter and Setter

--- Shoes.java
+++ Shoes.java
@@ -6,4 +6,35 @@ public class Shoes {
     private int size;
     private double price;
 
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
